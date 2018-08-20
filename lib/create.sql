7c734ef25af244c25a3ce215096adf9fcab70849
Achsah Abraham <achsahmp@gmail.com>
2018-08-20 21:41:50 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -8,4 +8,8 @@ CREATE TABLE projects (
 );
 
 CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
-CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id TEXT);
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id TEXT,
+  project_id TEXT);
