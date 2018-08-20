9695a7c24f5d496839760e8d700110fb7d5f53b6
Achsah Abraham <achsahmp@gmail.com>
2018-08-20 22:32:08 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,13 +18,18 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT name, age, SUM(pledges.amount)
 FROM users
-JOIN pledges on users.id = pledges.user_id
+INNER JOIN pledges on users.id = pledges.user_id
 GROUP BY user_id
 ORDER BY name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title, SUM(pledges.amount), projects.funding_goal
+FROM projects
+INNER JOIN projects.id = pledges.project_id
+WHERE pledges.amount > projects.funding_goal
+GROUP BY projects_id
+"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
